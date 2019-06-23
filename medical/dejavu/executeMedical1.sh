bd0884f43122704e8dc5de3b3984477b4bb3efce
Maximilian Voelker <maximilian.voelker@mailbox.org>
2019-06-23 17:15:23 +0000

    Change dejavu execution script to also save dejavu-results file

--- medical/dejavu/executeMedical1.sh
+++ medical/dejavu/executeMedical1.sh
@@ -3,4 +3,5 @@ for i in {1..9}
 do
   echo "Checking log $i"
   dejavu /root/OM/OM-git/medical/dejavu/medical1.qtl /root/OM/OM-git/medical/logs/dejavu/$i.log > /root/OM/OM-git/medical/results/dejavu/1/log$i.result
+  mv /root/OM/OM-git/medical/dejavu/dejavu-results /root/OM/OM-git/medical/results/dejavu/1/dejavu-$i.result
 done
