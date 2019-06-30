54287011d23f8d3ffe42a0293cfc3ce7cec7b4eb
Maximilian Voelker <maximilian.voelker@mailbox.org>
2019-06-30 19:08:41 +0000

    Extend medical monpoly run script to log 1 to 9

--- medical/monpoly/executeMedical1.sh
+++ medical/monpoly/executeMedical1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {9..5}
+for i in {1..9}
 do
   echo "Checking log $i"
   monpoly -sig /root/OM/OM-git/medical/monpoly/medical.sig -formula /root/OM/OM-git/medical/monpoly/medical1.mfotl -negate -log /root/OM/OM-git/medical/logs/monpoly/$i.log -stats -mem > /root/OM/OM-git/medical/results/monpoly/1/log$i.result
