b9accbaaca946d6b7ab4fd30614bcc517722d9df
Maximilian Voelker <maximilian.voelker@mailbox.org>
2019-07-20 13:08:43 +0000

    Add pidstat measurement to dejavu script

--- medical/dejavu/executeMedical1.sh
+++ medical/dejavu/executeMedical1.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+pidstat -C dejavu -d -r -H -h -p ALL 1 > dejavu.measurement &
+
 for i in {1..9}
 do
   echo "Checking log $i"
-  dejavu /root/OM/OM-git/medical/dejavu/medical1.qtl /root/OM/OM-git/medical/logs/dejavu/$i.log > /root/OM/OM-git/medical/results/dejavu/1/log$i.result
-  mv /root/OM/OM-git/medical/dejavu/dejavu-results /root/OM/OM-git/medical/results/dejavu/1/dejavu-$i.result
+  dejavu /root/OM/OM-git/medical/dejavu/medical1.qtl /root/OM/OM-git/medical/logs/dejavu/$i.log > /root/OM/OM-git/medical/results/dejavu/1/measurement_log$i.result
+  mv /root/OM/OM-git/medical/dejavu/dejavu-results /root/OM/OM-git/medical/results/dejavu/1/dejavu-measurement-$i.result
 done
+
+kill -s TERM `pgrep pidstat`
